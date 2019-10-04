fdf5747498b51712654fe738bd4cfd293b9951ee
Abhinav Gupta <agupta@cleo.com>
2019-10-04 13:58:26 +0530

    Installed AgGrid

--- highLigtTextInAgGrid/src/app/app.component.ts
+++ highLigtTextInAgGrid/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'highLigtTextInAgGrid';
+
+  columnDefs = [
+    {headerName: 'Make', field: 'make' },
+    {headerName: 'Model', field: 'model' },
+    {headerName: 'Price', field: 'price'}
+  ];
+  rowData: any;
+  constructor(private http: HttpClient){}
+
+  ngOnInit() {
+    this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
+  }
 }
